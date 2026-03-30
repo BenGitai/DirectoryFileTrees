476150c459aa2c33bbd26d10fec204de68bc712c
Jeremy Arking <ja8354@princeton.edu>
2026-03-30 19:39:42 -0400

    wrote logic for inserting dir

--- 3FT/ft.c
+++ 3FT/ft.c
@@ -35,10 +35,12 @@ static size_t ulCount;
 int DT_insertDir(const char *pcPath) {
    int iStatus;
    Path_T oPPath = NULL;
-   Node_T oNFirstNew = NULL;
    Dir_T oDCurr = NULL;
+   Dir_T oDNew = NULL;
    size_t ulDepth, ulIndex;
    size_t ulNewNodes = 0;
+   Path_T oPPrefix = NULL;
+   Dir_T oDNew = NULL;
 
    assert(pcPath != NULL);
 
@@ -51,9 +53,23 @@ int DT_insertDir(const char *pcPath) {
       return iStatus;
 
     ulDepth = Path_getDepth(oPPath);
-    ulIndex = 0;
+    ulIndex = 1;
     oDCurr = oDRoot;
-    while (ulIndex < ulDepth) {
+    /* handle the case where there is not a root already*/
+    if (oDRoot == NULL) {
+        iStatus = Path_prefix(oPPath, ulIndex, &oPPrefix);
+        if (iStatus != SUCCESS) {
+            Path_free(oPPath);
+            return iStatus;
+        }
+        oDRoot = Dir_new(oPPrefix, NULL);
+        if (oDRoot == NULL) {
+            return MEMORY_ERROR;
+        }
+        ulNewNodes++;
+        ulIndex++;
+    }
+    while (ulIndex <= ulDepth) {
         Path_T oPPrefix = NULL;
         Dir_T oDNew = NULL;
         iStatus = Path_prefix(oPPath, ulIndex, &oPPrefix);
@@ -61,98 +77,22 @@ int DT_insertDir(const char *pcPath) {
             Path_free(oPPath);
             return iStatus;
         }
-        if (oDCurr == NULL) {
-            oDRoot = Dir_new(oPPrefix, oDCurr);
-            if (oDRoot == NULL) {
+        if (Dir_hasFileChild(oDCurr, oPPrefix)) {
+            return NOT_A_Dir;
+        }
+        if (Dir_hasDirChild(oDCurr, oPPrefix)) {
+            if (ulIndex == ulDepth) {
+                return ALREADY_IN_TREE;
+            }
+        } else {
+            oDNew = Dir_new(oPPrefix, oDCurr);
+            if (oDNew == NULL) {
                 return MEMORY_ERROR;
             }
+            Dir_insertDir(oDCurr, ODnew);
             ulNewNodes++;
-        } else {
-            if (Dir_hasFileChild(oDCurr, oPPrefix)) {
-                return CONFLICTING_PATH;
-            }
-            if (!Dir_hasDirChild(oDCurr, oPPrefix)) {
-                oDNew = Dir_new(oPPrefix, oDCurr);
-                if (oDNew == NULL) {
-                    return MEMORY_ERROR;
-                }
-                Dir_insertDir(oDCurr, ODnew);
-                ulNewNodes++;
-            }
         }
         ulIndex++;
         oDCurr = oDnew;
     }
-
-   /* find the closest ancestor of oPPath already in the tree */
-   iStatus= DT_traversePath(oPPath, &oNCurr);
-   if(iStatus != SUCCESS)
-   {
-      Path_free(oPPath);
-      return iStatus;
-   }
-
-   /* no ancestor node found, so if root is not NULL,
-      pcPath isn't underneath root. */
-   if(oNCurr == NULL && oNRoot != NULL) {
-      Path_free(oPPath);
-      return CONFLICTING_PATH;
-   }
-
-   ulDepth = Path_getDepth(oPPath);
-   if(oNCurr == NULL) /* new root! */
-      ulIndex = 1;
-   else {
-      ulIndex = Path_getDepth(Node_getPath(oNCurr))+1;
-
-      /* oNCurr is the node we're trying to insert */
-      if(ulIndex == ulDepth+1 && !Path_comparePath(oPPath,
-                                       Node_getPath(oNCurr))) {
-         Path_free(oPPath);
-         return ALREADY_IN_TREE;
-      }
-   }
-
-   /* starting at oNCurr, build rest of the path one level at a time */
-   while(ulIndex <= ulDepth) {
-      Path_T oPPrefix = NULL;
-      Node_T oNNewNode = NULL;
-
-      /* generate a Path_T for this level */
-      iStatus = Path_prefix(oPPath, ulIndex, &oPPrefix);
-      if(iStatus != SUCCESS) {
-         Path_free(oPPath);
-         if(oNFirstNew != NULL)
-            (void) Node_free(oNFirstNew);
-         assert(CheckerDT_isValid(bIsInitialized, oNRoot, ulCount));
-         return iStatus;
-      }
-
-      /* insert the new node for this level */
-      iStatus = Node_new(oPPrefix, oNCurr, &oNNewNode);
-      if(iStatus != SUCCESS) {
-         Path_free(oPPath);
-         Path_free(oPPrefix);
-         if(oNFirstNew != NULL)
-            (void) Node_free(oNFirstNew);
-         assert(CheckerDT_isValid(bIsInitialized, oNRoot, ulCount));
-         return iStatus;
-      }
-      /* set up for next level */
-      Path_free(oPPrefix);
-      oNCurr = oNNewNode;
-      ulNewNodes++;
-      if(oNFirstNew == NULL)
-         oNFirstNew = oNCurr;
-      ulIndex++;
-   }
-
-   Path_free(oPPath);
-   /* update DT state variables to reflect insertion */
-   if(oNRoot == NULL)
-      oNRoot = oNFirstNew;
-   ulCount += ulNewNodes;
-
-   assert(CheckerDT_isValid(bIsInitialized, oNRoot, ulCount));
-   return SUCCESS;
 }
