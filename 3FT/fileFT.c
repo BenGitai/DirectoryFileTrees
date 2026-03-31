69576d8a646ddc2239b238f0e765144ae06b93f5
Benjamin Gitai <bengitai@gmail.com>
2026-03-31 16:57:05 -0400

    fileFT code
    First full fileFT

--- 3FT/fileFT.c
+++ 3FT/fileFT.c
@@ -12,16 +12,32 @@ struct file {
     void *contents;
 };
 
+/*
+  Compares the string representation of oNfirst with a string
+  pcSecond representing a node's path.
+  Returns <0, 0, or >0 if oNFirst is "less than", "equal to", or
+  "greater than" pcSecond, respectively.
+*/
+static int File_compareString(const File_T oFFirst,
+                                 const char *pcSecond) {
+   assert(oFFirst != NULL);
+   assert(pcSecond != NULL);
+
+   return Path_compareString(oFFirst->oPPath, pcSecond);
+}
 
 File_T File_new(Path_T oPPath, Dir_T oDParent, void *contents) {
-   File_T result = malloc(sizeof(struct file));
-   if (result == NULL) {
-       return NULL;
-   }
-   result->oPPath = oPPath;
-   result->oDParent = oDParent;
-   result->contents = contents;
-   return result;
+  assert(oPPath != NULL);
+  assert(oDParent != NULL);
+  
+  File_T result = malloc(sizeof(struct file));
+  if (result == NULL) {
+    return NULL;
+  }
+  result->oPPath = oPPath;
+  result->oDParent = oDParent;
+  result->contents = contents;
+  return result;
 }
 
 /*
@@ -29,47 +45,43 @@ File_T File_new(Path_T oPPath, Dir_T oDParent, void *contents) {
   oFFile, i.e., deletes this File and all its descendents. Returns the
   number of Files deleted.
 */
-size_t File_free(File_T oFFile);
+size_t File_free(File_T oFFile) {
+  assert(oFFile != NULL);
+
+  free(oFFile);
+
+  return 1;
+}
 
 /* Returns the path object representing oFFile's absolute path. */
-Path_T File_getPath(File_T oFFile);
+Path_T File_getPath(File_T oFFile) {
+  assert(oFFile != NULL);
 
-/*
-  Returns TRUE if oFParent has a child with path oPPath. Returns
-  FALSE if it does not.
+  return oFFile->oPPath;
+}
 
-  If oFParent has such a child, stores in *pulChildID the child's
-  identifier (as used in File_getChild). If oFParent does not have
-  such a child, stores in *pulChildID the identifier that such a
-  child _would_ have if inserted.
-*/
-boolean File_hasChild(File_T oFParent, Path_T oPPath,
-                         size_t *pulChildID);
-
-/* Returns the number of children that oFParent has. */
-size_t File_getNumChildren(File_T oFParent);
-
-/*
-  Returns an int SUCCESS status and sets *poFResult to be the child
-  File of oFParent with identifier ulChildID, if oFe exists.
-  Otherwise, sets *poFResult to NULL and returns status:
-  * NO_SUCH_PATH if ulChildID is not a valid child for oFParent
-*/
-int File_getChild(File_T oFParent, size_t ulChildID,
-                  File_T *poFResult);
 
 /*
   Returns a the parent File of oFFile.
   Returns NULL if oFFile is the root and thus has no parent.
 */
-File_T File_getParent(File_T oFFile);
+File_T File_getParent(File_T oFFile) {
+  assert(oFFile != NULL);
+
+  return oFFile->oDParent;
+}
 
 /*
   Compares oFFirst and oFSecoFd lexicographically based oF their paths.
   Returns <0, 0, or >0 if oFFirst is "less than", "equal to", or
   "greater than" oFSecoFd, respectively.
 */
-int File_compare(File_T oFFirst, File_T oFSecoFd);
+int File_compare(File_T oFFirst, File_T oFSecond) {
+  assert(oFFirst != NULL);
+  assert(oFSecond != NULL);
+
+  return Path_comparePath(oFFirst->oPPath, oFSecond->opPath);
+}
 
 /*
   Returns a string representatioF for oFFile, or NULL if
@@ -78,4 +90,14 @@ int File_compare(File_T oFFirst, File_T oFSecoFd);
   Allocates memory for the returned string, which is then owned by
   the caller!
 */
-char *File_toString(File_T oFFile);
+char *File_toString(File_T oFFile) {
+  char *copyPath;
+
+  assert(oFFile != NULL);
+
+  copyPath = malloc(Path_getStrLength(File_getPath(oFFile)) + 1);
+  if (copyPath == NULL)
+    return NULL;
+  else
+    return strcpy(copyPath, Path_getPathname(File_getPath(oFFile)));
+}
